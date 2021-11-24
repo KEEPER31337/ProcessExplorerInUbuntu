6e4b9bcb0fa44ef76d356ac5226e605d5a6e4f00
SongSeyeon <songsy405@pusan.ac.kr>
2021-11-25 00:07:15 +0900

    add sysinfo.h , processinfo's field

--- src/command.cpp
+++ src/command.cpp
@@ -26,27 +26,31 @@ using namespace std;
 Command::Command()
     :mMode(PRINTPROCINFO)
 {
+    msysinfo= new SysInfo();
+    setSysInfo();
+    cout<<msysinfo->uptime;
     mProcInfo = new vector<ProcInfo>;
 }
 
-void Command::setMode(Mode m)
-{
-    mMode = m;
-}
-Command::Mode Command::getMode(void) const
-{
-    return mMode;
-}
+void Command::setMode(Mode m) { mMode = m; }
+Command::Mode Command::getMode(void) const{ return mMode; }
 
-int Command::getProcNum()
+int Command::getProcNum(){ return mProcInfo->size(); }
+
+vector<ProcInfo> &Command::getProcInfo(void) const{ return *mProcInfo; }
+void Command::setSysInfo(void)  //sysinfo의 값 설정
 {
-    return mProcInfo->size();
+    FILE *fp;
+    double stime;
+    double idlettime;
+    ifstream ifs("/proc/uptime");
+    if(!ifs.is_open()){
+        cerr<<"cannot open /proc/uptime"<<endl;
+    }
+    ifs>>stime;
+    ifs.close();
+    msysinfo->uptime=stime;
 }
-
-vector<ProcInfo> &Command::getProcInfo(void) const{
-    return *mProcInfo;
-}
-
 void Command::updateProcStat(void)
 {
     DIR* dp;
@@ -68,17 +72,26 @@ void Command::updateProcStat(void)
             }
             ProcInfo ps;
             string s;
-            vector<string> t;
+            vector<string> t;   // /proc/[pid]/stat의 내용 파싱
             for (int i=0;i<40;i++){
                 ifs >> s;
                 t.push_back(s);
             }
-
+            //username 가져옴
+            struct passwd *upasswd;
+            struct stat lstat;
+            stat(strcat(direntry->d_name,"/stat"),&lstat);
+            upasswd=getpwuid(lstat.st_uid);
+    
+            ps.username = upasswd->pw_name;
             ps.pid   = stoi(t[0]);
-            ps.comm  = t[1];
-            ps.state = t[2][0];
             ps.ppid  = stoi(t[3]);
-
+            ps.state = t[2][0];
+            ps.comm  = t[1];
+            ps.cpu   = getCpuTime(stoi(t[13]), stoi(t[14]), stoi(t[21]), msysinfo->uptime);
+            ps.start = getStartTime(msysinfo->uptime, stoi(t[21]));
+            ps.vmem  = stoi(t[22]);
+            
             mProcInfo->push_back(ps);
 
             ifs.close();
@@ -90,14 +103,47 @@ void Command::updateProcStat(void)
     }
     
 }
+double Command::getCpuTime(ulong utime, ulong stime, ulong starttime, int seconds){
+    unsigned long long total_time;
+    int pcpu=0;
+    total_time = utime + stime; 
+    seconds = seconds - (int)(starttime/100.);
+    if(seconds){
+        pcpu = (int)(total_time * 1000ULL/100.)/seconds;
+    }
+    return pcpu/10.0;
+}
+string Command::getStartTime(ulong uptime,ulong stime){
+    char result[16];
+    unsigned int hertz=(unsigned int)sysconf(_SC_CLK_TCK);
+    long startT=time(NULL)-(uptime-(stime/hertz));
+    struct tm* tmStart=localtime(&startT);
+    if(time(NULL) - startT < 24 * 60 * 60){
+        strftime(result, 16, "%H:%M", tmStart);
+    }
+    else if(time(NULL) - startT < 7 * 24 * 60 * 60){
+        strftime(result, 16, "%b %d", tmStart);
+    }
+    else{
+        strftime(result, 16, "%y", tmStart);
+    }
+    string str(result);
+    return str;
+}
+
 void Command::printProc(void) const 
 {
-    printf("%7s %7s %2s %16s\n", "PID", "PPID", "ST", "NAME"); 
-    printf("===================================\n");
+    printf("%-10s %7s %7s %2s %16s %4s %9s %10s\n","USER", "PID", "PPID", "ST", "NAME", "CPU", "VMEM", "START"); 
+    printf("========================================================================\n");
     for (int i = 0; i < mProcInfo->size(); i++){
-        printf("%7d %7d %2c %16s\n",(*mProcInfo)[i].pid,
-                                    (*mProcInfo)[i].ppid,
-                                    (*mProcInfo)[i].state, 
-                                    (*mProcInfo)[i].comm.c_str());
+        printf("%-10s %7d %7d %2c %16s %2.1f %9d %10s\n",
+                            (*mProcInfo)[i].username.c_str(),
+                            (*mProcInfo)[i].pid,
+                            (*mProcInfo)[i].ppid,
+                            (*mProcInfo)[i].state, 
+                            (*mProcInfo)[i].comm.c_str(),
+                            (*mProcInfo)[i].cpu,
+                            (*mProcInfo)[i].vmem,
+                            (*mProcInfo)[i].start.c_str());
     }    
 }
