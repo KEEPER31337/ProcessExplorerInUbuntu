6459dfe50349bae1a89ce0cf8f86511aa9e79cd3
lcy8047 <lcy8047@gmail.com>
2021-11-25 00:18:10 +0900

    add mutex

--- src/cmdwindow.h
+++ src/cmdwindow.h
@@ -1,11 +1,12 @@
 #include "window.h"
 #include "command.h"
+#include <mutex>
 
 class CmdWindow : public Window
 {
 public:
     CmdWindow(int endY, int endX, int begY, int begX);
-    void startShell(void);
+    void startShell(std::mutex &mutPrintScr, std::mutex &mutGetch);
     
 private:
     
