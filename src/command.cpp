9e9a93913945725e9fd9cf6a81eac40c5adffe40
1004-head <woduf0628@naver.com>
2021-11-26 03:30:04 +0900

    add getprocinfobyid func in command

--- src/command.cpp
+++ src/command.cpp
@@ -226,3 +226,31 @@ void Command::RestartProc(int PID, std::string pathProc)
     strcpy(runCMD, pathProc.c_str());
     system(runCMD);
 }
+
+ProcInfo Command::GetProcInfoByPID(int PID)
+{
+    ProcInfo selecProc;
+
+    vector<ProcInfo>::iterator ptr;
+    for(ptr = mProcInfo->begin(); ptr != mProcInfo->end(); ++ptr)
+    {
+        if(ptr->pid == PID) 
+        {
+            selecProc.pid = ptr->pid;
+            selecProc.ppid = ptr->ppid;
+            selecProc.state = ptr->state;
+            selecProc.comm = ptr->comm;
+            selecProc.start = ptr->start;
+        }
+        else if(ptr->ppid == PID) 
+        {
+            selecProc.pid = ptr->pid;
+            selecProc.ppid = ptr->ppid;
+            selecProc.state = ptr->state;
+            selecProc.comm = ptr->comm;
+            selecProc.start = ptr->start;
+        }
+    }
+    
+    return selecProc;
+}
