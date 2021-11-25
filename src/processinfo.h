c64d59fe050b82a7aa90a9302869823ce2d9d957
lcy8047 <lcy8047@gmail.com>
2021-11-26 03:00:57 +0900

    modify sequence

--- src/processinfo.h
+++ src/processinfo.h
@@ -5,16 +5,14 @@
 
 using namespace std;
 
-struct ProcInfo
-{   
-    string username;    //프로세스 사용 유저명
-    double cpu;   //cpu 사용률
-    string start; //프로세스 시작 시간
-    int vmem;//메모리 점유율
-    
-    int pid;
-    int ppid;
-    char state;
-    string comm;    //(프로세스 이름)
-     
+struct ProcInfo {       
+    int     pid;        // process id
+    int     ppid;       // parent process id
+    string  comm;       // command
+    double  cpu;        // cpu 사용률
+    int     vmem;       // 메모리 점유율
+    char    state;      // state
+    string  username;   // 프로세스 사용 유저명
+    string  start;      // 프로세스 시작 시간
+                        // # of threads
 };
