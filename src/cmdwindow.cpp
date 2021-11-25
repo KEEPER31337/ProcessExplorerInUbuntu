f1da80581412b790c5c161b709353bb3f913e089
lcy8047 <lcy8047@gmail.com>
2021-11-26 04:57:40 +0900

    add printHelp

--- src/cmdwindow.cpp
+++ src/cmdwindow.cpp
@@ -7,7 +7,16 @@
 
 CmdWindow::CmdWindow(int endY, int endX, int begY, int begX)
     : Window(endY, endX, begY, begX)
+    , mCmd(new Command())
 {
+    mCmdEntry = new vector<CommandEntry>;
+
+    mCmdEntry->push_back(CommandEntry{"info",        "info [PID] - display the information of processs' detail informain"});
+    mCmdEntry->push_back(CommandEntry{"path",        "path [PID] - display the path of each process"});
+    mCmdEntry->push_back(CommandEntry{"viruscheck",  "viruscheck [PID] - check a status which process is infection"});
+    mCmdEntry->push_back(CommandEntry{"kill",        "kill [PID] [SGINAL_NUM] - send a signal to process"});
+    mCmdEntry->push_back(CommandEntry{"search",      "search [PID] [KEYWORD] - search the processes using keyword"});
+    mCmdEntry->push_back(CommandEntry{"help",        "print help"});
 }
 
 void CmdWindow::StartShell(std::mutex &mutPrintScr, std::mutex &mutGetch)
@@ -45,7 +54,7 @@ void CmdWindow::StartShell(std::mutex &mutPrintScr, std::mutex &mutGetch)
             break;
 
         case '\n':
-            printArgs(s);
+            executeCommand(s);
             wprintw(mWindow, "> ");
             s.clear();
             bPrevSpace = false;
@@ -62,6 +71,86 @@ void CmdWindow::StartShell(std::mutex &mutPrintScr, std::mutex &mutGetch)
     }
 }
 
+void CmdWindow::executeCommand(string &args)
+{
+    initArgList(args.c_str());
+    char buf[1024];
+    int argCount = 0;
+    string cmd;
+    vector<CommandEntry>::iterator it;
+    lineFeed();
+    if ( getNextArg(buf) == 0 ) {
+        printStr("wrong input");
+        return;
+    }
+    cmd = buf;
+
+    for ( it=mCmdEntry->begin(); it != mCmdEntry->end(); it++ ) {
+        if( cmd.compare(it->cmd) == 0 ) {
+
+            if( cmd.compare("help") == 0 )
+                executeHelp();
+            break;
+
+        }
+    }
+    if ( it == mCmdEntry->end() )
+        printStr("not found command");
+}
+
+/*
+void CmdWindow::executeInfo(void)
+{
+    int pid;
+    char buf[1024];
+
+    if ( getNextArg(buf) == 0 ) {
+        printStr("wrong input");
+        return;
+    }
+    pid = stoi(buf);
+
+    mCmd->
+
+}
+
+void CmdWindow::executePath(void)
+{
+ 
+}
+
+void CmdWindow::executeVirusCheck(void)
+{
+    
+}
+
+void CmdWindow::executeKill(void)
+{
+    
+}
+
+void CmdWindow::executeSearch(void)
+{
+    
+}
+*/
+
+void CmdWindow::executeHelp(void)
+{
+    vector<CommandEntry>::iterator it;
+    for ( it=mCmdEntry->begin(); it != mCmdEntry->end(); it++ ) {
+        printStr(it->help);
+    }
+}
+
+void CmdWindow::lineFeed(void)
+{
+    if (getcury(mWindow) == getmaxy(mWindow) - 1)
+        lineClear();
+    else
+        wmove(mWindow, getcury(mWindow) + 1, 0);
+}
+
 // require string new line (x == 0)
 void CmdWindow::printStr(string s)
 {
@@ -72,10 +161,7 @@ void CmdWindow::printStr(string s)
         mvwaddnstr(mWindow, getcury(mWindow), 0, s.c_str()+curIdx, getmaxx(mWindow));
 
         curIdx += getmaxx(mWindow);
-        if (getcury(mWindow) == getmaxy(mWindow) - 1)
-            lineClear();
-        else
-            wmove(mWindow, getcury(mWindow) + 1, 0);
+        lineFeed();
     }
 }
 
@@ -85,11 +171,7 @@ int CmdWindow::printArgs(string input)
     char buf[1024];
     int argCount = 0;
 
-    if (getcury(mWindow) == getmaxy(mWindow) - 1)
-        lineClear();
-    else
-        wmove(mWindow, getcury(mWindow) + 1, 0);
-
+    lineFeed();
     while (getNextArg(buf) != 0)
     {
         stringstream ss;
--- src/cmdwindow.h
+++ src/cmdwindow.h
@@ -2,6 +2,11 @@
 #include "command.h"
 #include <mutex>
 
+typedef struct CommandEntry {
+    string cmd;
+    string help;
+} CommandEntry;
+
 class CmdWindow : public Window
 {
 public:
@@ -9,11 +14,14 @@ public:
     void StartShell(std::mutex &mutPrintScr, std::mutex &mutGetch);
     
 private:
-    void printStr    (string s);
-    int  printArgs   (std::string input);
-    void lineClear   (void);
-    void initArgList (string args);
-    int  getNextArg  (char *arg);
+    void executeCommand (string &args);
+    void executeHelp    (void);
+    void lineFeed       (void);
+    void printStr       (string s);
+    int  printArgs      (std::string input);
+    void lineClear      (void);
+    void initArgList    (string args);
+    int  getNextArg     (char *arg);
     
     struct argList {
         std::string argBuffer;
@@ -21,5 +29,6 @@ private:
         int curArgc;
     } arglist;
 
-    Command mCmd;
+    vector<CommandEntry> *mCmdEntry;
+    Command *mCmd;
 };
