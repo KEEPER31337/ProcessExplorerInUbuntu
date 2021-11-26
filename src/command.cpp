254e3a3fcbcc2aaeffd72860c4c4b1ae25314b53
lcy8047 <lcy8047@gmail.com>
2021-11-26 10:37:19 +0900

    add virus total api

--- src/command.cpp
+++ src/command.cpp
@@ -14,6 +14,14 @@
 #include <sstream>
 #include <limits.h>
 
+// virustotal
+#include <iomanip>
+#include <openssl/bio.h>
+#include <openssl/hmac.h>
+#include <openssl/buffer.h>
+#include <curl/curl.h>
+#include "json/json.h"
+
 // for debug
 #include <cassert>
 
@@ -105,13 +113,13 @@ void Command::UpdateProcStat(void)
                 }
             }
             
-            ps.pid      = stoi(t[0]);
-            ps.ppid     = stoi(t[3]);
+            ps.pid      = stoul(t[0]);
+            ps.ppid     = stoul(t[3]);
             ps.state    = t[2][0];
             ps.comm     = t[1];
-            ps.cpu      = getCpuTime(stoi(t[13]), stoi(t[14]), stoi(t[21]), mSysInfo->uptime);
-            ps.start    = getStartTime(mSysInfo->uptime, stoi(t[21]));
-            ps.vmem     = stoi(t[22]);
+            ps.cpu      = getCpuTime(stoul(t[13]), stoul(t[14]), stoul(t[21]), mSysInfo->uptime);
+            ps.start    = getStartTime(mSysInfo->uptime, stoul(t[21]));
+            ps.vmem     = stoul(t[22]);
 
             mProcInfo->push_back(ps);
 
@@ -285,8 +293,209 @@ string Command::GetProcPath(int pid)
 
     len = readlink(ss.str().c_str(), buf, sizeof(buf)-1);
     if ( len == -1 ) {
-        // error
+        return string("cannot get path");
     }
     buf[len] = '\0';
     return string(buf);
+}
+
+// for virustotal
+string Command::GetVirusTotalReport(int pid)
+{
+    std::stringstream result;
+    std::string filepath;
+    Json::Value scanResult;
+    Json::Value reportResult;
+    Json::Value scanList;
+    std::string fileName;
+    int detectedCnt = 0;
+    int totalCnt    = 0;
+
+    filepath = GetProcPath(pid);
+    scanResult = parsingJson( requestScan(filepath) );
+    scanList = parsingJson( requestReport(scanResult["resource"].asCString()) )["scans"];
+    totalCnt = scanList.size();
+
+    for (Json::ValueIterator it = scanList.begin(); it != scanList.end(); it++) {
+        if ((*it)["detected"].asString() != "false") {
+            detectedCnt++;
+        }
+    }
+
+    result << "Virus Detected(detected / total) : "
+           << detectedCnt << " / " << totalCnt;
+
+    return result.str();
+}
+
+fileData &Command::getFileData(std::string &fileName)
+{
+    std::ifstream ifs(fileName, std::ifstream::binary);
+    fileData *filedata = new fileData();
+    int length;
+
+    ifs.seekg(0, ifs.end);
+    length = ifs.tellg();
+    ifs.seekg(0, ifs.beg);
+
+    filedata->data = new char[length];
+    filedata->length = length;
+
+    ifs.read(filedata->data, length);
+
+    return *filedata;
+}
+
+std::string &Command::base64(fileData &filedata)
+{
+    std::string *s = new std::string();
+    BIO *bmem, *b64;
+    BUF_MEM *bptr;
+
+    b64 = BIO_new(BIO_f_base64());
+    bmem = BIO_new(BIO_s_mem());
+    b64 = BIO_push(b64, bmem);
+
+    BIO_write(b64, filedata.data, filedata.length);
+    BIO_flush(b64);
+    BIO_get_mem_ptr(b64, &bptr);
+
+    *s = bptr->data;
+
+    return *s;
+}
+
+static size_t writeMemory(char *data, size_t size, size_t nmemb, std::string *s)
+{
+    if (data == NULL)
+        return 0;
+
+    s->append(data, size * nmemb);
+
+    return size * nmemb;
+}
+
+std::string &Command::makeScanPostFields(std::string &fileName)
+{
+    fileData filedata = getFileData(fileName);
+
+    std::string *postFields = new std::string();
+    *postFields = "apikey=";
+    *postFields += APIKEY;
+    *postFields += "&file=data:application/octet-stream;";
+    *postFields += "name=hashtest;";
+    *postFields += "base64,";
+    *postFields += base64(filedata);
+
+    return *postFields;
+}
+
+std::string &Command::makeReportGetURL(std::string &resource)
+{
+    std::string *url = new std::string();
+
+    *url  = VIRUSTOTAL_URL;
+    *url += "file/report";
+    *url += "?apikey=";
+    *url += APIKEY;
+    *url += "&resource=";
+    *url += resource;
+    *url += "&allinfo=false";
+
+    return *url;
+}
+
+std::string &Command::requestReport(std::string resource)
+{
+    CURL *curl;
+    CURLcode code;
+    std::string *s = new std::string();
+
+    curl = curl_easy_init();
+    if (curl == NULL)
+    {
+        std::cerr << "curl init error" << std::endl;
+    }
+    code = curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+    if (code != CURLE_OK)
+        std::cerr << "CustomRequest Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_setopt(curl, CURLOPT_URL, makeReportGetURL(resource).c_str());
+    if (code != CURLE_OK)
+        std::cerr << "URL Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeMemory);
+    if (code != CURLE_OK)
+        std::cerr << "WriteFunction Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, s);
+    if (code != CURLE_OK)
+        std::cerr << "WriteData Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_perform(curl);
+    if (code != CURLE_OK)
+        std::cerr << "curl_easy_perform Error : " << curl_easy_strerror(code) << std::endl;
+
+    curl_easy_cleanup(curl);
+
+    return *s;
+}
+
+std::string &Command::requestScan(std::string &fileName)
+{
+    CURL *curl;
+    CURLcode code;
+    std::string *s = new std::string();
+
+    curl = curl_easy_init();
+    if( curl == NULL ) {
+        std::cerr << "curl init error" << std::endl;
+    }
+    code = curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+    if ( code != CURLE_OK )
+        std::cerr << "CustomRequest Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_setopt(curl, CURLOPT_URL, VIRUSTOTAL_SCAN_URL.c_str());
+    if ( code != CURLE_OK )
+        std::cerr << "URL Error : " << curl_easy_strerror(code) << std::endl;
+    struct curl_slist *headers = NULL;
+
+    headers = curl_slist_append(headers, "Accept: application/json");
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+
+    code = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    if ( code != CURLE_OK )
+        std::cerr << "HttpHeader Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_setopt(curl, CURLOPT_POSTFIELDS, makeScanPostFields(fileName).c_str());
+    if ( code != CURLE_OK )
+        std::cerr << "PostFields Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeMemory);
+    if ( code != CURLE_OK )
+        std::cerr << "WriteFunction Error : " << curl_easy_strerror(code) << std::endl;
+    
+    code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, s);
+    if ( code != CURLE_OK )
+        std::cerr << "WriteData Error : " << curl_easy_strerror(code) << std::endl;
+
+    code = curl_easy_perform(curl);
+    if ( code != CURLE_OK )
+        std::cerr << "curl_easy_perform Error : " << curl_easy_strerror(code) << std::endl;
+
+    curl_easy_cleanup(curl);
+
+    return *s;
+}
+
+Json::Value &Command::parsingJson(std::string &data)
+{
+    Json::Value *rootr = new Json::Value();
+    Json::Reader reader;
+
+    if ( !reader.parse(data, *rootr) ) {
+        std::cerr << "json parse error" << std::endl;
+    }
+
+    return *rootr;
 }
--- src/command.h
+++ src/command.h
@@ -1,12 +1,21 @@
 #include "processinfo.h"
 #include "sysinfo.h"
-//#include "search.h"
+#include "json/json.h"
 
 #pragma once
 
 using namespace std;
 
 typedef unsigned long long ULL;
+
+typedef struct fileData {
+    char *data;
+    unsigned long long length;
+} fileData;
+
+const std::string VIRUSTOTAL_URL = "https://www.virustotal.com/vtapi/v2/";
+const std::string VIRUSTOTAL_SCAN_URL = "https://www.virustotal.com/vtapi/v2/file/scan";
+const std::string APIKEY = "717e5ae38a274a05ecebbdb08291a3350fdbde97b29b1ff03666c4d95d543a3a";
 
 class Command
 {
@@ -31,6 +40,7 @@ public:
     void SendSignal(int pid, int signalNum);
     vector<ProcInfo> &SearchProc(vector<ProcInfo> &procInfo, string procAttr, string proc);
     string GetProcPath(int pid);
+    string GetVirusTotalReport(int pid);
 
 private:
     // for update proc status
@@ -39,7 +49,19 @@ private:
     string getStartTime(ULL uptime, ULL stime);
     string getUserName(char* path);
 
+    //for virus total
+    fileData &getFileData(std::string &fileName);
+    std::string &base64(fileData &filedata);
+    
+    std::string &makeScanPostFields(std::string &fileName);
+    std::string &makeReportGetURL(std::string &resource);
+    std::string &requestReport(std::string resource);
+    std::string &requestScan(std::string &fileName);
+    Json::Value &parsingJson(std::string &data);
+
     SysInfo *mSysInfo;
     vector<ProcInfo> *mProcInfo;
     Mode mMode;
 };
+
+static size_t writeMemory(char *data, size_t size, size_t nmemb, std::string *s);
