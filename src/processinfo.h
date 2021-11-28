5da044fa1a954cb464f12586aa372ca7def252d5
lcy8047 <lcy8047@gmail.com>
2021-11-29 01:20:57 +0900

    add uid euid, modify cpu usage print precision

--- src/processinfo.h
+++ src/processinfo.h
@@ -13,16 +13,17 @@ struct ProcInfo {
     string procInfoToStrWithName(void)
     {
         stringstream ss;
-        ss << "PID : "   << pid   << ", "
-        << "PPID : "     << ppid  << ", "
-        << "CPU : "      << cpu   << ", "
-        << "MEM : "      << vmem  << ", "
-        << "STAT : "     << state << ", "
-        << "USER : "     << user  << ", "
-        << "START : "    << start << ", "
-        << "THREADS : "  << nlwp  << ", "
-        << "COMMAND : "  << comm  << ", "
-        //<< "PNAME : "    << name    << ", "
+        ss << "PID : "      << pid      << ", "
+           << "PPID : "     << ppid     << ", ";
+        ss.precision(1);
+        ss << fixed
+           << "CPU : "      << cpu      << ", "
+           << "MEM : "      << vmem     << ", "
+           << "STAT : "     << state    << ", "
+           << "USER : "     << user     << ", "
+           << "RUNTIME : "  << start    << ", "
+           << "THREADS : "  << threads  << ", "
+           << "COMMAND : "  << comm     << ", "
         ;
 
         return ss.str();
@@ -32,15 +33,16 @@ struct ProcInfo {
     {
         stringstream ss;
         ss << setw(8)  << pid  
-           << setw(8)  << ppid 
+           << setw(8)  << ppid;
+        ss.precision(1);
+        ss << fixed
            << setw(8)  << cpu  
            << setw(12) << vmem 
-           << setw(5)  << state
+           << setw(8)  << state
            << setw(8)  << user 
-           << setw(8)  << start
-           << setw(8)  << nlwp 
+           << setw(10) << start
+           << setw(8)  << threads 
            << setw(20) << comm 
-           //<< setw(20) << name 
         ;
 
         return ss.str();
@@ -54,6 +56,7 @@ struct ProcInfo {
     char    state;      // state
     string  user;       // 프로세스 사용 유저명
     string  start;      // 프로세스 시작 시간
-    int     nlwp;       // # of threads (number of light weight process)
-    //string  name;       // process name
+    int     threads;    // # of threads (number of light weight process)
+    int     uid;        // user id
+    int     euid;       // effective user id
 };
